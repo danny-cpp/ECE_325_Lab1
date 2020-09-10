d51c294afe032958267805fd39009f3f4969b9ea
Danh Nguyen <65102457+danny-cpp@users.noreply.github.com>
2020-09-10 00:10:51 -0400

    added sort

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("helloWorld!");
+        System.out.println(5/2);
     }
 }
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -12,9 +12,28 @@ public class MergeSort {
      * @param numbers   {@code int[]} The integer array to be sorted
      */
     public static int[] sort(int[] numbers) {
-        // TODO: Lab 1 -- write mergesort here
+        // This is a recursive sorting algorithm, we need a base case that break recursive loop
+        // If the array is length 1, it is already sorted
+        if (numbers.length == 1) {
+            return numbers;
+        }
 
-	return numbers;
+        // We cache the length here for ease of use
+        int total_length = numbers.length;
+        int left_length = total_length/2; // This is integer division (round down)
+        int right_length = total_length - left_length; // By doing this, even if total left is odd, this would solve the problem
+
+        // If they array is not yet split to length 1, recursively call split...
+        int[] ArrayLeft = new int[left_length];
+        int[] ArrayRight = new int[right_length];
+
+        // The idea of merge sort is: Assume that we have 2 sorted lists, we merge them to make 1 sorted list
+        // so, why don't we merge sort the sub lists, then merge? This is recursive fundamental
+        ArrayLeft = sort(ArrayLeft);
+        ArrayRight = sort(ArrayRight);
+
+        // Dont forget to return the merged version
+        return merge(ArrayLeft, ArrayRight);
     }
 
     /**
